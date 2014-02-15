e1c4d77c26bd3cdbd0e27f73429b61a68bffc6a2
Tyler Wasden <twasden@gmail.com>
2014-02-15 14:34:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+League::Application.config.secret_key_base = 'c00f7bb6e57cb1a9b8b1d4192168f61cf8f8a93a1d4f0a008cb750d31cd187687db68905090ef56023f48ce4192011b144e0ff106a6cbae4ebe7427c3c4de54b'
